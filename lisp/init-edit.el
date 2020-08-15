4db768f455c556fdfe3c6b597706e076e58b9422
keer2345 <keer2345@gmail.com>
2020-08-15 09:55:54 +0800

    avy-goto-char-2

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -9,7 +9,8 @@
 
 (use-package avy
   :bind (("C-:" . avy-goto-char)
-         ("C-'" . avy-goto-char-2)
+         ;; ("C-'" . avy-goto-char-2)
+         ("C-;" . avy-goto-char-2)
          ("M-g f" . avy-goto-line)
          ("M-g w" . avy-goto-word-1)
          ("M-g e" . avy-goto-word-0))
